5ad21b3d071fdeb0f0f99f639e605434a2d083e3
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-07-16 17:50:57 -0400

    Fix sum test json bug

--- DevopsPractice.Tests.Behavior/Steps/SumSteps.cs
+++ DevopsPractice.Tests.Behavior/Steps/SumSteps.cs
@@ -5,17 +5,22 @@ using Xunit;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace DevopsPractice.Tests.Behavior.Steps
 {
     [Binding]
     public class SumSteps
     {
+        class JsonResponse{
+            public string result { get; set; }
+        }
+
         private HttpClient _client;
         private const string BASE_URL = "https://localhost:5001/";
         private int num1;
         private int num2;
-        private string content;
+        private JsonResponse content;
 
 
         [Given(@"I input numbers (.*) and (.*)")]
@@ -32,13 +37,15 @@ namespace DevopsPractice.Tests.Behavior.Steps
             _client = appFactory.CreateClient();
 
             var response = await _client.GetAsync($"{BASE_URL}calculator/sum?num1={num1}&num2={num2}");
-            content = await response.Content.ReadAsStringAsync();
+            var jsonString = await response.Content.ReadAsStringAsync();
+            content = JsonConvert.DeserializeObject<JsonResponse>(jsonString);
+            
         }
         
         [Then(@"the result (.*) should be the sum")]
         public void ThenTheResultShouldBeTheSum(string result)
         {
-            Assert.Equal(result, content);
+            Assert.Equal(result, content.result);
         }
     }
 }
