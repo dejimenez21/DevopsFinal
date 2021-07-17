abc727bf8ddb266cc408f73981671780e54ef885
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-07-17 03:31:51 -0400

    Join sum steps and subtract steps in a unique file

--- DevopsPractice.Tests.Behavior/Steps/CalculateSteps.cs
+++ DevopsPractice.Tests.Behavior/Steps/CalculateSteps.cs
@@ -10,26 +10,28 @@ using Newtonsoft.Json;
 namespace DevopsPractice.Tests.Behavior.Steps
 {
     [Binding]
-    public class SumSteps
+    public class CalculateSteps
     {
-        class JsonResponse{
+        protected class JsonResponse{
             public string result { get; set; }
         }
 
-        private HttpClient _client;
-        private const string BASE_URL = "https://localhost:5001/";
-        private int num1;
-        private int num2;
-        private JsonResponse content;
+        protected HttpClient _client;
+        protected const string BASE_URL = "https://localhost:5001/";
+        protected int num1;
+        protected int num2;
+        protected JsonResponse content;
 
-
+        #region GenericSteps
         [Given(@"I input numbers (.*) and (.*)")]
         public void GivenIInputNumbersAnd(int a, int b)
         {
             num1 = a;
             num2 = b;
         }
+        # endregion
         
+        #region SumSteps
         [When(@"I send sum request")]
         public async Task WhenISendSumRequest()
         {
@@ -47,5 +49,26 @@ namespace DevopsPractice.Tests.Behavior.Steps
         {
             Assert.Equal(result, content.result);
         }
+        #endregion
+
+        #region SubtractSteps
+        [When(@"I send subtract request")]
+        public async Task WhenISendSubtractRequest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient();
+
+            var response = await _client.GetAsync($"{BASE_URL}calculator/subtract?num1={num1}&num2={num2}");
+            var jsonString = await response.Content.ReadAsStringAsync();
+            content = JsonConvert.DeserializeObject<JsonResponse>(jsonString);
+            
+        }
+        
+        [Then(@"the result (.*) should be the subtraction")]
+        public void ThenTheResultShouldBeTheSubtraction(string result)
+        {
+            Assert.Equal(result, content.result);
+        }
+        #endregion
     }
 }
